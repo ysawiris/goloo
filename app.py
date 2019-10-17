7fcd62246aa9a426ce863dfc536ed19822fa1899
Youssef_Sawiris <youssef.sawiris@students.makeschool.com>
2019-10-16 21:19:04 -0700

    create review route

--- app.py
+++ app.py
@@ -14,12 +14,27 @@ db = client.get_default_database()
 restroom = db.restroom
 reviews = db.reviews 
 
-
-
 @app.route('/')
 def hompage():
     """Show Hompage"""
     return render_template('base.html')
+
+@app.route('/reviews')
+def show_reviews():
+    """Show Recommedations"""
+    return render_template('show_reviews.html', reviews = reviews.find())
+
+@app.route('/reviews/create_new', methods=['POST'])
+def create_review():
+    """Create a New Review"""
+    review = {
+        'title': request.form.get('title'),
+        'name': request.form.get('name'),
+        'content': request.form.get('content'),
+    }
+    print(review)
+    reviews.insert_one(review)
+    
 
 
 if __name__ == "__main__":
